ac77de6d7cf01af96f225f1f58006bd1c861a20c
Dane Enright <enrightco@comcast.net>
2021-06-16 13:13:05 -0600

    added delete and get to api.js

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,31 +2,48 @@ const router = require("express").Router();
 const Workout = require("../models/exercise.js");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create(req.body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }) 
-    .catch(err => {
-        res.status(400).json(err);
+  Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
-router.put("/api/workouts/:id", ({body, params}, res) => {
-    Workout.findByIdAndUpdate(
-        params.id,
-        { $push: {exercises: body}},
-        {new: true , runValidators: true}
-    )
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }) 
-    .catch(err => {
-        res.status(400).json(err);
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
-})
+});
 
+router.get("/api/workouts", (req, res) => {
+    Workout.get(req.body)
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 
+    })
 
-
+router.delete("api/workouts", (req, res) => {
+  Workout.delete(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
