71f332799fbd7e124e0a5692d51d7bbcd5c34ef8
Dane Enright <enrightco@comcast.net>
2021-06-16 23:33:00 -0600

    added api/workouts/range

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -35,7 +35,7 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-// router.get("/api/workouts", (req, res) => {
+// router.get("/api/workouts/range", (req, res) => {
 //   Workouts.find({})
 //     .then((dbWorkout) => {
 //       res.json(dbWorkout);
